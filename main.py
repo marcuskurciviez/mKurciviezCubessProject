fe5a8f99e37913f8c6f311395a5bcfdb57353a0d
johns <jsantore@bridgew.edu>
2023-02-07 15:26:29 -0500

    test comment for workflow

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import json
 
 """This function serves the purpose of the GET request to the wufoo form, using the secrets key to retrieve the 
 form submissions. Runs through a if statement for a status code if any errors occur."""
-def get_wufoo_data() -> dict:
+def get_wufoo_data() -> dict:  # comment to test workflow
     url = "http://mkurciviez.wufoo.com/api/v3/forms/cubess-project-proposal-submission/entries/json"
+    url = "https://jsantore.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries/json"
     response = requests.get(url, auth=HTTPBasicAuth(secrets.wufoo_key, 'pass'))
     print(response.text)
 
